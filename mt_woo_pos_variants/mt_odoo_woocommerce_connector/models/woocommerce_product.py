a501132a6e447127ee3a31df48043c9f73f2a781
Mouzoune <151739403+Mouzoune@users.noreply.github.com>
2025-06-26 22:41:59 +0100

    admin_env

--- mt_woo_pos_variants/mt_odoo_woocommerce_connector/models/woocommerce_product.py
+++ mt_woo_pos_variants/mt_odoo_woocommerce_connector/models/woocommerce_product.py
@@ -297,8 +297,9 @@ class Product(models.Model):
             # self.env(user=2)
             admin_env = self.env(user=1)
             _logger.error(f"Write it ???? === {self.env.user}")
+            _logger.error(f"Write it ????admin_env === {admin_env}")
 
-            super(Product, admin_env.sudo()).write(values)
+            super(Product, admin_env).write(values)
             _logger.error(f"Write it ????11=== {self.env.user}")
 
             _logger.error(f'WRITE METHOD WITH:  self.env.context.get dont_send_data_to_wooc_from_write_method')
