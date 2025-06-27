375dfcf8d2ba9f506c72f749792e91412897d057
Mouzoune <151739403+Mouzoune@users.noreply.github.com>
2025-06-27 14:51:49 +0100

    .with_user(self.env.ref("base.user_admin"))

--- mt_woo_pos_variants/mt_odoo_woocommerce_connector/controllers/main.py
+++ mt_woo_pos_variants/mt_odoo_woocommerce_connector/controllers/main.py
@@ -135,8 +135,8 @@ class Main(http.Controller):
             product = woo_api.get(url, params=params)
             product_data_item = product.json()
 
-            admin_env = request.env(user=1)
-            admin_env['product.template'].sudo().with_context(dont_send_data_to_wooc_from_write_method=True).create_product(product_data_item[0], wooc_instance)
+            # admin_env = request.env(user=1)
+            request.env['product.template'].with_user(self.env.ref("base.user_admin")).sudo().with_context(dont_send_data_to_wooc_from_write_method=True).create_product(product_data_item[0], wooc_instance)
         return {'status': 'success', 'message': 'Product processed successfully'}
 
 
