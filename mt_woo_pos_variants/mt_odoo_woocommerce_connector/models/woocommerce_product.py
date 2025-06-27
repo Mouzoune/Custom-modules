e7218ab251aa0330a4c88963125b2206cc7d1cde
Mouzoune <151739403+Mouzoune@users.noreply.github.com>
2025-06-27 05:09:14 +0100

    with_context

--- mt_woo_pos_variants/mt_odoo_woocommerce_connector/models/woocommerce_product.py
+++ mt_woo_pos_variants/mt_odoo_woocommerce_connector/models/woocommerce_product.py
@@ -270,12 +270,12 @@ class Product(models.Model):
         ctx = dict(self.env.context)
         _logger.error(f"Values it 000 === {values}")
         _logger.error(f"Write it 000 === {self.env.user}")
-        if self.env.context.get("dont_send_data_to_wooc_from_write_method"):
+        # if self.env.context.get("dont_send_data_to_wooc_from_write_method"):
             # user_admin = self.sudo().env.ref("base.user_admin")
             # context = user_admin.context_get()
             # self.env(user=2)
-            group = self.sudo().env.ref('stock.group_stock_user')
-            self.sudo().env.user.write({'groups_id': [(4, group.id)]})
+            # group = self.sudo().env.ref('stock.group_stock_user')
+            # self.sudo().env.user.write({'groups_id': [(4, group.id)]})
 
         _logger.error(f'self env context =====> {self.env.context.get("dont_send_data_to_wooc_from_write_method")}')
         if not values.get('taxes_id'):
@@ -310,7 +310,6 @@ class Product(models.Model):
 
                 super(Product, self.with_user(SUPERUSER_ID).sudo()).write(values)
                 _logger.error(f"Write it ????11=== {self.env.user}")
-                self.sudo().env.user.write({'groups_id': [(3, group.id)]})
                 _logger.error(f'WRITE METHOD WITH:  self.env.context.get dont_send_data_to_wooc_from_write_method')
             else:
                 super(Product, self.sudo()).write(values)
@@ -531,7 +530,7 @@ class Product(models.Model):
         else:
             _logger.error(f'/product////////////// {product} ---')
 
-            product.sudo().with_context(dont_send_data_to_wooc_from_write_method=True).write(dict_p)
+            # product.sudo().with_context(dont_send_data_to_wooc_from_write_method=True).write(dict_p)
         _logger.error('/22222////////////// dont_send_data_to_wooc_from_write_method ---')
 
         # self.env.cr.commit()
